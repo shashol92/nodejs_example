c8e7e30a8378285d2a34edf5bdbd40919ad115f2
shashikumarOL <shashikumar.ol@sap.com>
2021-09-07 21:40:55 +0530

    added pug view

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express();
+app.set('view engine', 'pug');
+app.set('views', './views');
 
 app.get('/api', (req, res) => {
     res.send('Hello......')
 });
 
+app.get('/first_template', (req, res) => {
+    res.render('first_template');
+})
+
 app.listen(5000, ()=> {
     console.log(`server running on port number 5000`)
 });
+
